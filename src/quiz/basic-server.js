6650421d0c95ce234a0a5268fa76c7691aafb8c1
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-02-22 16:18:03 -0500

    fixed basic-server to send bad response with incomplete parameters

--- src/quiz/basic-server.js
+++ src/quiz/basic-server.js
@@ -28,23 +28,35 @@ const incidents = [
     }
 ]
 
-const server = http.createServer((req, res) => {
+
+/**
+ * Note: if the URI is missing parameters, you dont get a 404 error, you just get a (perpetually) pending request. 
+ * Change the code to handle a request with missing parameters in the request.
+ * e.g. localhost:8000/incidents/A90/stonehaven/1 instead of
+ *      localhost:8000/incidents/A90/stonehaven/north/1 (parameter missing = north!)
+ */
+
+
+const server = http.createServer((req, res) => {  // the function that we write here takes only a request and a response. The request (header) must state if GET/PUT/POST/DELETE.
     // Parse the request URL to extract parameters
-    const parsedUrl = url.parse(req.url, true);
+    const parsedUrl = url.parse(req.url, true);     // the server catches the request, and parses the URI. Here we parse the URI to extract the parameters from the URL/URI
     const { pathname } = parsedUrl;
 
     // Check if the request is for the specified endpoint
-    if (pathname.startsWith('/incidents/')) {
+    if (pathname.startsWith('/incidents/')) {   // then we check it. Is this the expected URI for incidents?? This string will be part of request header that the client sends
         // Extract parameters from the URL
-        const [, , road, location, direction, id] = pathname.split('/');
+        const [, , road, location, direction, id] = pathname.split('/');  // then we check if it has the corresponding properties!
 
         // Check if all parameters are provided
-        if (road && location && direction && id) {
+        if (road && location && direction && id) {  // if it does, then send response. 
             // Send response with the extracted parameters
-            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.writeHead(200, { 'Content-Type': 'application/json' }); // set some meta information for the response header, 200 means VALID!
             res.end(JSON.stringify(incidents.filter((incident) => {
                 return incident.id === `MABOS00${id}`
-            })));
+            }))); // need to add the else part here! If not all parameters are provided, then we need a response header with status code!!
+        } else { // in order to avoid a pending request indefinitely, we need to tell it what code to send if invalid
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Endpoint not found.'); // re-start the server to see if this works and changes what happens with incomplete parameters!
         }
     } else {
         // If the request is for an unsupported endpoint, send a not found response
