b63fd74b55c7bfecc19bd9e4e00fc9ea8cf3ab1a
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-02-29 15:32:32 -0500

    class activity re: async await

--- src/quiz/animations/main.js
+++ src/quiz/animations/main.js
@@ -13,19 +13,31 @@ const aliceTumbling = [
   const alice2 = document.querySelector("#alice2");
   const alice3 = document.querySelector("#alice3");
 
-  alice1
-    .animate(aliceTumbling, aliceTiming)
-    .finished
-    .then((res) => {
-        console.log(res);
-        alice2
-            .animate(aliceTumbling, aliceTiming)
-            .finished
-            .then((res) => {
-                console.log(res);
-                alice3.animate(aliceTumbling, aliceTiming);
-            })
-    });
+  // add the asynch/await code here!
+  async function animate() {
+    try{
+      await alice1.animate(aliceTumbling, aliceTiming).finished;
+      await alice2.animate(aliceTumbling, aliceTiming).finished;
+      await alice3.animate(aliceTumbling, aliceTiming).finished;
+    }
+    catch(err) {
+      console.log(`Error when animating: ${err.message}`)
+    }
+  }
+
+  // alice1
+  //   .animate(aliceTumbling, aliceTiming)
+  //   .finished
+  //   .then((res) => {
+  //       console.log(res);
+  //       alice2
+  //           .animate(aliceTumbling, aliceTiming)
+  //           .finished
+  //           .then((res) => {
+  //               console.log(res);
+  //               alice3.animate(aliceTumbling, aliceTiming);
+  //           })
+  //   });
 
   // Promise chain  
   // alice1.animate(aliceTumbling, aliceTiming).finished  
